d95014e4c1b8b88f765c5b0a94bb970950c3cedc
zm0042082-debug <zm0042082@gmail.com>
2026-08-15 03:07:24 +0300

    Add additional print statements for string indexing and slicing examples

--- lesson-11/code.py
+++ lesson-11/code.py
@@ -1,2 +1,12 @@
 name="Zeyad mahmoud"
-print(name[2:10])
+print(name[2]) 
+name="Zeyad mahmoud"
+print(name[-2]) 
+name="Zeyad mahmoud"
+print(name[-2:-4]) 
+name="Zeyad mahmoud"
+print(name[-2:10]) 
+name="Zeyad mahmoud"
+print(name[0:10])
+name="Zeyad mahmoud"
+print(name[2:12])
