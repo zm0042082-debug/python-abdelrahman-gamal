3779796d738100c115f4ca1b717994cc01984722
zm0042082-debug <zm0042082@gmail.com>
2026-08-21 03:22:13 +0300

    Add lesson 15, 16, and 17 code and README files

---
+++ lesson-15/code.py
@@ -0,0 +1,9 @@
+print(12)
+print(10+5)
+print(10-5)
+print(12*10)
+print(12/12)
+
+print(type(12))
+print(type(12.5))
+print(type(12+9j))
---
+++ lesson-16/code.py
@@ -0,0 +1,9 @@
+print("25+4"+"years")
+print("24+58"+"years")
+print(str(25)+"years")
+
+
+
+print(float(1))
+print(int(1.5))
+print(complex(10))
