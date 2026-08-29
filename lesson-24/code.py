1bb0967913d3ff3a809d932fb7fe4e1c2cb40d9a
zm0042082-debug <zm0042082@gmail.com>
2026-08-29 04:02:24 +0300

    Add lesson 24 and lesson 25 README and code files

---
+++ lesson-24/code.py
@@ -0,0 +1,34 @@
+info = {
+"name"     : "Zeyad" ,
+ "age"     :  23  ,
+"country"   : "egypt"
+}
+print(info)
+
+info = {
+"name"     : "Zeyad" ,
+ "age"     :  23  ,
+"country"   : "egypt"
+}
+print(info["name"])
+
+info={ 
+    "name"     : "Zeyad" ,
+     "age"     :  23  ,
+   "country"   : "egypt"
+   }
+print(info.get("country"))
+
+info={ 
+    "name"     : "Zeyad" ,
+     "age"     :  23  ,
+   "country"   : "egypt"
+   }
+print(info.get("id"))
+
+info={ 
+    "name"     : "Zeyad" ,
+     "age"     :  23  ,
+   "country"   : "egypt"
+   }
+print(info.get("id","not found"))
